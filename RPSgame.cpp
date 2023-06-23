54d3930f192ad087f897ebc2a98ebce377a47ff0
jarju <jarjudrag@gmail.com>
2023-06-23 23:34:51 +0530

    Added

---
+++ RPSgame.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <random>
+
+using namespace std;
+
+char UserMove();
+char ComputerMove();
+char ShowTheInput();
+char WhoIsTheWinner();
+
+int main(){
+
+  char user, mac;
+  UserMove();
+
+
+}
+
+char UserMove()
+{
+  
+  std::cout << "---------------------------\n";
+  std::cout << "  ROCK - PAPER - SCISSORS"<< endl;
+  std::cout << "---------------------------\n"<<endl;
+
+  return 0;
+}
+
+char ComputerMove()
+{
+  return 0;
+}
+
+char ShowTheInput()
+{
+  return 0;
+}
+
+char WhoIsTheWinner()
+{
+  return 0;
+}
